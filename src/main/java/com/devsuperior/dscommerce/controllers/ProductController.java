7b7287c077758e97c38067fedb3a1c153bccfae3
MatheusThierri <matheusthierri7@gmail.com>
2025-09-23 21:22:00 -0300

    Criação da API

---
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -0,0 +1,24 @@
+package com.devsuperior.dscommerce.controllers;
+
+import com.devsuperior.dscommerce.dto.ProductDTO;
+import com.devsuperior.dscommerce.entities.Product;
+import com.devsuperior.dscommerce.repositories.ProductRepository;
+import com.devsuperior.dscommerce.services.ProductService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+    private ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/{id}")
+    public ProductDTO getProduct(@PathVariable Long id) {
+        return productService.findById(id);
+    }
+}
